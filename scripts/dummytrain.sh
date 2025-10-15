859135732d6b3e1d526eb9a2a5b4aa9435f4088c
unknown <jchavan010@gmail.com>
2025-10-15 15:47:05 -0400

    Arcee2-celeba256 experiment script

--- scripts/dummytrain.sh
+++ scripts/dummytrain.sh
@@ -1,24 +1,59 @@
-torchrun --standalone --nproc_per_node=1 ../Arcee/train.py --exp 4test --datadir ../data/celeba256/lmdb_new --dataset celeba_256 --eval-refdir ../data/celeba256/real_samples \
+export WANDB_API_KEY='2f92f218fe46708930c460c6f57055ac6ce1361c'
+export CUDA_VISIBLE_DEVICES=0
+
+# to start a brand new run 
+# rm -rf run_state/$EXP
+# rm -rf results/"$EXP"/
+# comment out --resume flag
+EXP="checking-errors"
+STATE_DIR="run_state/${EXP}"
+RUN_ID_FILE="${STATE_DIR}/wandb_run_id.txt"
+mkdir -p "${STATE_DIR}"
+# Resume if we already have a saved run
+if [[ -f "${RUN_ID_FILE}" ]]; then
+  export WANDB_RUN_ID="$(
+  <"$RUN_ID_FILE"
+  )"
+  export WANDB_RESUME="must"
+else
+  export WANDB_RUN_ID="$(
+python - <<'PY'
+from wandb.util import generate_id
+print(generate_id())
+PY
+)"
+  echo "$WANDB_RUN_ID" > "${RUN_ID_FILE}"
+  export WANDB_RESUME="allow"
+fi
+
+NUM_GPUS=1
+BATCH_SIZE=8
+EVAL_BS=4
+
+GLOBAL_BATCH_SIZE=$((BATCH_SIZE * NUM_GPUS))
+
+torchrun --standalone --nproc_per_node=$NUM_GPUS ../Arcee/train.py --exp $EXP --datadir ../data/celeba256/lmdb_new --dataset celeba_256 --eval-refdir ../data/celeba256/real_samples \
   --image-size 256 \
   --num-classes 1 \
   --block-type normal \
-  --model Zigma-XS/2 \
-  --scan-type Zigma_2 \
+  --model Arcee-B/2 \
+  --scan-type Arcee_8 \
   --ssm-dstate 256 \
-  --train-steps 5000 \
-  --eval-every 40001 \
-  --plot-every 6 \
+  --train-steps 60000 \
+  --eval-every 5000 \
+  --plot-every 500 \
   --ckpt-every 10000 \
-  --log-every 1 \
-  --global-batch-size 1 \
-  --sample-bs 1 \
-  --eval-bs 1 \
-  --lr 1e-4 \
+  --log-every 5 \
+  --global-batch-size $GLOBAL_BATCH_SIZE \
+  --sample-bs $BATCH_SIZE \
+  --eval-bs $EVAL_BS \
+  --lr 3e-4 \
   --learnable-pe \
   --path-type GVP \
-  --eval-nsamples 1 \
+  --eval-nsamples 10000 \
   --rms-norm \
   --fused-add-norm \
   --drop-path 0.0 \
-  --save-content-every 50000
-  # --use-wandb
+  --save-content-every 10000 \
+  --use-wandb \
+  #--resume \
